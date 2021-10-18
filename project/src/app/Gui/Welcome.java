f021c109aa2db709a3dfccb4cbe3a54950ed36d0
Toni <antonio.moldovan82@yahoo.com>
2021-10-18 22:42:15 +0300

    Finished login part

--- project/src/app/Gui/Welcome.java
+++ project/src/app/Gui/Welcome.java
@@ -1,4 +1,6 @@
 package app.Gui;
+import app.Classes.User;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +11,9 @@ public class Welcome implements ActionListener {
 
     JTextField usernameField;
     JButton submitButton;
+
+    //User part
+    public String username = "";
 
     public Welcome(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,12 +31,18 @@ public class Welcome implements ActionListener {
         usernameLabel.setForeground(new Color(212, 170, 125));
         usernameLabel.setBounds(45, 100, 340, 100);
 
+        JLabel usernameLabel2 = new JLabel();
+        usernameLabel2.setText("(16 characters maximum)");
+        usernameLabel2.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+        usernameLabel2.setForeground(new Color(212, 170, 125));
+        usernameLabel2.setBounds(75, 140, 340, 100);
+
         usernameField = new JTextField();
         usernameField.setBackground(new Color(75, 75, 75));
         usernameField.setForeground(new Color(212, 170, 125));
         usernameField.setHorizontalAlignment(JTextField.CENTER);
         usernameField.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
-        usernameField.setBounds(60, 190, 200, 35);
+        usernameField.setBounds(60, 250, 200, 35);
 
         submitButton = new JButton();
         submitButton.setText("Submit");
@@ -42,10 +53,8 @@ public class Welcome implements ActionListener {
         submitButton.setFocusable(false);
         submitButton.addActionListener(this);
 
-
-
-
         frame.add(usernameLabel);
+        frame.add(usernameLabel2);
         frame.add(usernameField);
         frame.add(submitButton);
         frame.setVisible(true);
@@ -55,7 +64,16 @@ public class Welcome implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == submitButton) {
-            System.out.println("Welcome: " + usernameField.getText());
+            if (usernameField.getText().length() <= 16) {
+                this.username = usernameField.getText();
+                User user = new User(this.username);
+                Frame mainFrame = new Frame();
+                frame.setVisible(false);
+                System.out.println("[LOG][Welcome.java]: User " + this.username + " created succesfully!");
+            }
+            else {
+                TooManyCharacters error = new TooManyCharacters();
+            }
         }
     }
 }
